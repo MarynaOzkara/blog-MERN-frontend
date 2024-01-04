6a14ce7c70a73c8866d2a3059fa7e4b8f1216073
MarynaOzkara <ozkaramaryna@gmail.com>
2024-01-04 22:38:51 +0200

    starting

---
+++ src/pages/FullPost.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+
+import { Post } from "../components/Post";
+import { Index } from "../components/AddComment";
+import { CommentsBlock } from "../components/CommentsBlock";
+import { useParams } from "react-router-dom";
+import axios from "../redux/axios";
+
+export const FullPost = () => {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const { id } = useParams();
+  // console.log(params);
+  useEffect(() => {
+    axios
+      .get(`/posts/${id}`)
+      .then((res) => {
+        console.log(res);
+        setData(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении статьи");
+      });
+  }, [id]);
+  if (isLoading) {
+    return <Post isLoading={isLoading} />;
+  }
+  return (
+    <>
+      <Post
+        id={data._id}
+        title={data.title}
+        imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+        user={data.author}
+        createdAt={data.createdAt}
+        viewsCount={data.viewsCount}
+        commentsCount={data.commentsCount}
+        tags={data.tags}
+        isFullPost
+      >
+        <p>{data.text}</p>
+      </Post>
+      <CommentsBlock
+        items={[
+          {
+            user: {
+              fullName: "Вася Пупкин",
+              avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
+            },
+            text: "Это тестовый комментарий 555555",
+          },
+          {
+            user: {
+              fullName: "Иван Иванов",
+              avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
+            },
+            text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
+          },
+        ]}
+        isLoading={false}
+      >
+        <Index />
+      </CommentsBlock>
+    </>
+  );
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Grid from "@mui/material/Grid";
+
+import { Post } from "../components/Post";
+import { TagsBlock } from "../components/TagsBlock";
+import { CommentsBlock } from "../components/CommentsBlock";
+import { useEffect } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPosts, fetchTags } from "../redux/slices/postSlice";
+
+export const Home = () => {
+  const { posts, tags } = useSelector((state) => state.posts);
+  const isPostsLoading = posts.ststus === "loading";
+  const isTagsLoading = tags.ststus === "loading";
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchPosts());
+    dispatch(fetchTags());
+  }, [dispatch]);
+  return (
+    <>
+      <Tabs
+        style={{ marginBottom: 15 }}
+        value={0}
+        aria-label="basic tabs example"
+      >
+        <Tab label="Новые" />
+        <Tab label="Популярные" />
+      </Tabs>
+      <Grid container spacing={4}>
+        <Grid xs={8} item>
+          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
+            isPostsLoading ? (
+              <Post isLoading={true} key={index} />
+            ) : (
+              <Post
+                id={obj._id}
+                title={obj.title}
+                imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+                user={obj.author}
+                createdAt={obj.createdAt}
+                viewsCount={obj.viewsCount}
+                commentsCount={obj.commentsCount}
+                tags={obj.tags}
+                isEditable
+              />
+            )
+          )}
+        </Grid>
+        <Grid xs={4} item>
+          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
+          <CommentsBlock
+            items={[
+              {
+                user: {
+                  fullName: "Вася Пупкин",
+                  avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
+                },
+                text: "Это тестовый комментарий",
+              },
+              {
+                user: {
+                  fullName: "Иван Иванов",
+                  avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
+                },
+                text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
+              },
+            ]}
+            isLoading={false}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
